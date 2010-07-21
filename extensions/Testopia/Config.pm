fee92be49d4c880870063bc338d70dd36dc5dfa5
ghendricks@novell.com <>
2010-07-21 12:45:17 -0600

    Missing strings in install Config

--- extensions/Testopia/Config.pm
+++ extensions/Testopia/Config.pm
@@ -25,25 +25,25 @@ use constant OPTIONAL_MODULES => [
         package => 'Text-CSV',
         module  => 'Text::CSV',
         version => '1.06',
-        feature => ['CSV Importing of test cases']
+        feature => ['testopia_csv_import']
     },
     {
         package => 'XML Schema Validator',
         module  => 'XML::Validator::Schema',
         version => '1.10',
-        feature => ['XML Importing of test cases and plans']
+        feature => ['testopia_xml_import']
     },
     {
         package => 'XML Schema Parser',
         module  => 'XML::SAX::ParserFactory',
         version => 0,
-        feature => ['XML Importing of test cases and plans']
+        feature => ['testopia_xml_import']
     },
     {
         package => 'XML Twig',
         module  => 'XML::Twig',
         version => 0,
-        feature => ['XML Importing of test cases and plans']
+        feature => ['testopia_xml_import']
     }
 ];    
 
